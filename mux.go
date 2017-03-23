c47b3168e286aac9a828d7d7dba5d4845abdf054
John Rothfels <john@sourcegraph.com>
2017-03-23 14:35:39 -0700

    test commit

--- mux.go
+++ mux.go
@@ -14,6 +14,7 @@ import (
 )
 
 // NewRouter returns a new router instance.
+// Hello World!
 func NewRouter() *Router {
 	return &Router{namedRoutes: make(map[string]*Route), KeepContext: false}
 }
